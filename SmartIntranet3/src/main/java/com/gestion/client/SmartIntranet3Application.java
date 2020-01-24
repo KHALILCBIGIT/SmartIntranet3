c197be081e685d12ecc849d06bd3db24a65c9e58
KhalilKertaoui <54622656+KhalilKertaoui@users.noreply.github.com>
2020-01-24 16:44:48 +0100

    First Commit of a azure App

---
+++ SmartIntranet3/src/main/java/com/gestion/client/SmartIntranet3Application.java
@@ -0,0 +1,13 @@
+package com.gestion.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIntranet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIntranet3Application.class, args);
+	}
+
+}
